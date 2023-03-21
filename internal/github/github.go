db305b6ee55be63aca78b9b7adf660ac7610fe2f
Massimiliano Giovagnoli <me@maxgio.it>
2023-03-21 11:32:03 +0100

    clean(internal/github): cleanup unused functions
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- internal/github/github.go
+++ internal/github/github.go
@@ -5,21 +5,19 @@ import (
 	"fmt"
 	"os"
 
-	gogit "github.com/go-git/go-git/v5"
 	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 	"k8s.io/test-infra/pkg/flagutil"
 	prow "k8s.io/test-infra/prow/flagutil"
 	gitv2 "k8s.io/test-infra/prow/git/v2"
-	"k8s.io/test-infra/prow/github"
 )
 
 type GitHubOptions struct {
-	prow.GitHubOptions
-
 	Username string
 
 	DryRun bool
+
+	prow.GitHubOptions
 }
 
 func (o *GitHubOptions) AddPFlags(pfs *pflag.FlagSet) {
@@ -43,14 +41,6 @@ func (o *GitHubOptions) ValidateAll() error {
 	return nil
 }
 
-func (o *GitHubOptions) BuildClient() (github.Client, error) {
-	githubClient, err := o.GitHubClient(o.DryRun)
-	if err != nil {
-		return nil, errors.Wrap(err, "error creating github client")
-	}
-	return githubClient, nil
-}
-
 func (o *GitHubOptions) GetGitClientFactory() (gitv2.ClientFactory, error) {
 	s := ""
 	factory, err := o.GitClientFactory("", &s, o.DryRun)
@@ -59,11 +49,4 @@ func (o *GitHubOptions) GetGitClientFactory() (gitv2.ClientFactory, error) {
 	}
 
 	return factory, nil
-}
-
-func (o *GitHubOptions) Clone(org, repo string) (*gogit.Repository, error) {
-	return gogit.PlainClone("", false, &gogit.CloneOptions{
-		URL:      fmt.Sprintf("https://github.com/%s/%s", org, repo),
-		Progress: nil,
-	})
 }
